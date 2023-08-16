0f1d88a54dcc86b2a7f9614b075f5a26e21fdc1e
Douglas Berkley <douglasmberkley@gmail.com>
2023-08-16 12:42:01 +0900

    added stimulus controller to handle total price

---
+++ app/javascript/controllers/booking_price_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="booking-price"
+export default class extends Controller {
+  static targets = ["start_date", "end_date", "price"];
+
+  connect() {}
+
+  update() {
+    const pricePerDay = parseInt(this.priceTarget.dataset.price, 10);
+    if (
+      this.start_dateTarget.value !== "" &&
+      this.end_dateTarget.value !== ""
+    ) {
+      const diffInMs =
+        new Date(this.end_dateTarget.value) -
+        new Date(this.start_dateTarget.value);
+      const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+      this.priceTarget.innerText = `¥${(
+        diffInDays * pricePerDay
+      ).toLocaleString()}`;
+    }
+
+    // else {
+    // this.priceTarget.innerText = `¥${pricePerDay} / day`;
+    // }
+  }
+}
